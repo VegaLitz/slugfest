a7faee96737cfb430df54106bfeef3cb0381589c
VegaLitz <71658023+VegaLitz@users.noreply.github.com>
2026-06-27 22:15:59 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC51VUmQXznPbzBRxsZZpPaQlhI3V91jWA",
-  authDomain: "slugfest-b0d88.firebaseapp.com",
-  databaseURL: "https://slugfest-b0d88-default-rtdb.firebaseio.com",
-  projectId: "slugfest-b0d88",
-  storageBucket: "slugfest-b0d88.firebasestorage.app",
-  messagingSenderId: "251949892763",
-  appId: "1:251949892763:web:f9718de7b507d57775b23e"
-};
